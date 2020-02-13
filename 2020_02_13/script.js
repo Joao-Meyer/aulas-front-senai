f4f5129bf9688d7e68e316543cd03058533e99ae
JOÃO HENRIQUE PARIZOTI MEYER <19267271@CFP127ALUNO.LOCAL>
2020-02-13 08:47:49 -0300

    Pré atualização das funções exibir e ocultar.

---
+++ 2020_02_13/script.js
@@ -0,0 +1,50 @@
+const $calcular = document.getElementById('calcular');
+const $exibir = document.getElementById( 'exibir' );
+const $ocultar = document.getElementById( 'ocultar' );
+const $conteiner = document.querySelector( '.conteiner' );
+
+const calcularMedia = ( n1, n2 ) => (parseInt(n1) + parseInt(n2) ) / 2;
+
+const verificarSituacao = ( media ) => media >= 5 ? "Aprovado" : "Reprovado";
+
+
+const exibirMedia = () => {
+    const $nome = document.getElementById('nome');
+    const nota1 = document.getElementById('nota1').value;
+    const nota2 = document.getElementById('nota2').value;
+    const $media = document.getElementById('media');
+    const $situacao = document.getElementById('situacao');
+
+    const media = calcularMedia ( nota1, nota2 );
+
+    $situacao.value = verificarSituacao ( media );
+
+    $media.value = media;
+
+}
+
+const defineConceito =  () => {
+    const media = document.getElementById ('media').value
+    const $conceito = document.getElementById ('conceito');
+
+    if ( media < 3 ) {
+        $conceito.value = "E";
+    }else if ( media < 5 ) {
+        $conceito.value = "D";
+    }else if ( media < 8 ) {
+        $conceito.value = "C";
+    }else if ( media < 10) {
+        $conceito.value = "B";
+    }else {
+        $conceito.value = "A";
+    }
+}
+
+const calcular = () => {
+    exibirMedia();
+    calcularConceito();
+}
+
+$calcular.addEventListener('click', calcular);
+
+const exibir = ( elemento ) => elemento.classList.add( 'exibir' );
