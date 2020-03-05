2ae3f10709383f30f72e18c780bf3000cb137fc2
JOÃO HENRIQUE PARIZOTI MEYER <19267271@CFP127ALUNO.LOCAL>
2020-03-05 11:29:50 -0300

    Inicio do exercicio do semaforo.

--- 2020_03_05/semaforo/semaforo.js
+++ 2020_03_05/semaforo/semaforo.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const $semaforo = document.getElementById( 'img' );
+const $vermelho = document.getElementById( 'vermelho' );
+const $amarelo = document.getElementById( 'amarelo' );
+const $verde = document.getElementById( 'verde' );
+const $automatico = document.getElementById( 'automatico' );
+
+const alteraImagem = ( element, nomeImagem ) => element.src = nomeImagem;
+
+const ligarVermelho = () => alteraImagem( $semaforo, './img/vermelho.png' );
