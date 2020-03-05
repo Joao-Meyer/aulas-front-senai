1ecfd547f4cd2fdae4eb9736f6b636da5dffbef2
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-03-05 17:25:35 -0300

    Atualização no exercício do semáforo.

--- 2020_03_05/semaforo/semaforo.js
+++ 2020_03_05/semaforo/semaforo.js
@@ -9,3 +9,35 @@ const $automatico = document.getElementById( 'automatico' );
 const alteraImagem = ( element, nomeImagem ) => element.src = nomeImagem;
 
 const ligarVermelho = () => alteraImagem( $semaforo, './img/vermelho.png' );
+const ligarAmarelo = () => alteraImagem( $semaforo, './img/amarelo.png' );
+const ligarVerde = () => alteraImagem( $semaforo, './img/verde.png' );
+
+let estaVermelho = false;
+let estaAmarelo = false;
+let estaVerde = false;
+
+const automatico = () => {
+    if( estaVerde == true ){
+        ligarAmarelo();
+    }
+    else if( estaAmarelo == true ){
+        ligarVermelho();
+    }
+    else {
+        ligarVerde();
+    }
+}
+
+let idAutomatico;
+
+const ligarAutomatico = () => {
+    if( idAutomatico === undefined ){
+        idAutomatico = setInterval( automatico, 1000 );
+    }
+}
+
+$vermelho.addEventListener( 'click', ligarVermelho );
+$amarelo.addEventListener( 'click', ligarAmarelo );
+$verde.addEventListener( 'click', ligarVerde );
+
+$automatico.addEventListener( 'click', ligarAutomatico );
