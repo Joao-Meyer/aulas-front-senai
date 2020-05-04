facc95eb78ff748dbff065115a06c90ca96fd774
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-05-04 20:36:26 -0300

    Início do desevolvimento do consumo da API pelo Javascript.

--- 2020_04_29_-_Projeto_SuperHeroApi/javascript/javaescrito.js
+++ 2020_04_29_-_Projeto_SuperHeroApi/javascript/javaescrito.js
@@ -1,1 +1,96 @@
-console.log("Oi");
+"use strict";
+
+const heroInfo = [{
+    "id": "",
+    "name": "<div class='spinner blue'></div>",
+    "powerstats": {
+        "intelligence": "<div class='spinner green'></div>",
+        "strength": "<div class='spinner green'></div>",
+        "speed": "<div class='spinner green'></div>",
+        "durability": "<div class='spinner green'></div>",
+        "power": "<div class='spinner green'></div>",
+        "combat": "<div class='spinner green'></div>"
+    },
+    "biography": {
+        "full-name": "<div class='spinner green'></div>",
+        "alter-egos": "<div class='spinner green'></div>",
+        "aliases": {
+            "0": "<div class='spinner green'></div>"
+        },
+        "place-of-birth": "<div class='spinner green'></div>",
+        "first-appearence": "<div class='spinner green'></div>",
+        "publisher": "<div class='spinner green'></div>",
+        "alignment": "<div class='spinner green'></div>"
+    },
+    "appearence": {
+        "gender": "<div class='spinner green'></div>",
+        "race": "<div class='spinner green'></div>",
+        "height": {
+            "0": "<div class='spinner green'></div>",
+            "1": "<div class='spinner green'></div>"
+        },
+        "weight": {
+            "0": "<div class='spinner green'></div>",
+            "1": "<div class='spinner green'></div>"
+        },
+        "eye-color": "<div class='spinner green'></div>",
+        "hair-color": "<div class='spinner green'></div>"
+    },
+    "work": {
+        "occupation": "<div class='spinner green'></div>",
+        "base": "<div class='spinner green'></div>"
+    },
+    "connections": {
+        "group-affiliation": "<div class='spinner green'></div>",
+        "relatives": "<div class='spinner green'></div>"
+    },
+    "image": {
+        "url": "<div class='spinner orange'></div>"
+    }
+}];
+
+const url = "https://www.superheroapi.com/api.php/1697582160383693/search/";
+
+const infoFill = () => {
+    let heroName = document.getElementById( 'conteinerNomeHeroi' );
+    let heroImage = document.getElementById( 'conteinerImagemHeroi' );
+    let heroPowerStats = document.getElementById( 'powerStats' );
+    let heroAppearence = document.getElementById( 'appearence' );
+    let heroConnections = document.getElementById( 'connections' );
+    let heroWork = document.getElementById( 'work' );
+    let heroBiography = document.getElementById( "biography" );
+    // heroImage.style.backgroundImage = heroInfo[0].image.url;
+
+    heroName.innerHTML = heroInfo[0].name;
+    heroImage.innerHTML = heroInfo[0].image.url;
+    // heroPowerStats.innerHTML = heroInfo[0].powerstats.combat;
+    // heroAppearence.innerHTML = heroInfo[0].appearence.race;
+    // heroConnections.innerHTML = heroInfo[0].connections.relatives;
+    // heroWork.innerHTML = heroInfo[0].work.occupation;
+    // heroBiography.innerHTML = heroInfo[0].biography["full-name"];
+
+    //Preenche Power Stats
+    for (var property in heroInfo[0].powerstats){
+        heroPowerStats.innerHTML += heroInfo[0].powerstats[property];
+    }
+    //Preenche Appearence
+    for (var property in heroInfo[0].appearence){
+        heroAppearence.innerHTML += heroInfo[0].appearence[property];
+    }
+    //Preenche Connections
+    for (var property in heroInfo[0].connections){
+        heroConnections.innerHTML += heroInfo[0].connections[property];
+    }
+    //Preenche Work
+    for (var property in heroInfo[0].work){
+        heroWork.innerHTML += heroInfo[0].work[property];
+    }
+    //Preenche Biography
+    for (var property in heroInfo[0].biography){
+        heroBiography.innerHTML += heroInfo[0].biography[property];
+    }
+}
+
+infoFill();
+
+console.log( heroInfo );
